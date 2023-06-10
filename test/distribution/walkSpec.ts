64b314e76f01b1fc3c08e66a2fc35ce8ae8c9026
Michael L Perry <michael@qedcode.com>
2023-06-09 21:12:08 -0500

    Create walk builder for easier testing

--- test/distribution/walkSpec.ts
+++ test/distribution/walkSpec.ts
@@ -1,4 +1,4 @@
-import { Walk, walkFromSpecification } from "../../src/distribution/walk";
+import { Walk, WalkStep, walkFromSpecification } from "../../src/distribution/walk";
 import { Company, Office, model } from "../model";
 
 describe("walkFromSpecification", () => {
@@ -10,21 +10,60 @@ describe("walkFromSpecification", () => {
 
     const walk = walkFromSpecification(specification);
 
-    const expected: Walk = {
-      steps: [
-        {
-          direction: "successor",
-          role: {
-            successorType: "Office",
-            name: "company",
-            predecessorType: "Company"
-          },
-          next: {
-            steps: []
-          }
-        }
-      ]
-    };
+    const expected = walkFrom("Company")
+      .successor("company", "Office")
+      .build();
     expect(walk).toEqual(expected);
   });
 });
+
+class WalkBuilder {
+  constructor(
+    private type: string,
+    private steps: WalkStep[]
+  ) { }
+
+  successor(name: string, successorType: string, then?: (builder: WalkBuilder) => WalkBuilder): WalkBuilder {
+    const builder = new WalkBuilder(successorType, []);
+    const next = then ? then(builder) : builder;
+    return new WalkBuilder(this.type, [
+      ...this.steps,
+      {
+        direction: "successor",
+        role: {
+          successorType,
+          name,
+          predecessorType: this.type
+        },
+        next: next.build()
+      }
+    ]);
+  }
+
+  predecessor(name: string, predecessorType: string, then?: (builder: WalkBuilder) => WalkBuilder): WalkBuilder {
+    const builder = new WalkBuilder(predecessorType, []);
+    const next = then ? then(builder) : builder;
+    return new WalkBuilder(this.type, [
+      ...this.steps,
+      {
+        direction: "predecessor",
+        role: {
+          successorType: this.type,
+          name,
+          predecessorType
+        },
+        next: next.build()
+      }
+    ]);
+  }
+
+  build(): Walk {
+    return {
+      steps: this.steps
+    };
+  }
+}
+
+function walkFrom(type: string): WalkBuilder {
+  return new WalkBuilder(type, []);
+}
